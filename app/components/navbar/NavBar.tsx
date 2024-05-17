ff44d6345f396958cc614aef4fbf15109533cf5d
Yurifull <yurifull.evolution@gmail.com>
2024-05-17 13:04:18 -0400

    feat(commit inicial)

---
+++ app/components/navbar/NavBar.tsx
@@ -0,0 +1,10 @@
+import { HomeIcon } from "@primer/octicons-react"
+
+export const NavBar = () => {
+  return (
+    <div>
+        NavBar
+        <HomeIcon></HomeIcon>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center p-24">
+      <span className="text-ellipsis">hola mundo</span>
+    </main>
+  );
+}
